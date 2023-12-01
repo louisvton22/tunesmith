fd9b3a7aba86f696145f76acdbd363a15d801a88
louisvton22 <81780806+louisvton22@users.noreply.github.com>
2023-11-30 17:03:29 -0800

    Login authentication error handling implemented

--- app/src/main/java/edu/ischool/lton2/tunesmith/MainActivity.kt
+++ app/src/main/java/edu/ischool/lton2/tunesmith/MainActivity.kt
@@ -1,14 +1,21 @@
 package edu.ischool.lton2.tunesmith
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.android.appremote.api.error.NotLoggedInException
+import com.spotify.android.appremote.api.error.UserNotAuthorizedException
 
 import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.error.SpotifyAppRemoteException
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
 
@@ -24,11 +31,53 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        this.runOnUiThread {
-            val connectionParams = ConnectionParams.Builder(clientId)
-                .setRedirectUri(redirectUri)
-                .showAuthView(true)
-                .build()
+        findViewById<Button>(R.id.btnConnect).setOnClickListener{ authorizeUser() }
+
+
+        Log.i(TAG, "Showing Auth Screen")
+
+
+
+
+    }
+    fun handleException(error: SpotifyAppRemoteException) {
+        val alert = AlertDialog.Builder(this)
+
+        alert.setTitle("Linking Error")
+        when (error) {
+            is NotLoggedInException -> {
+                alert.setMessage("Please Login onto the spotify")
+                alert.setPositiveButton("Go") { dialog, which ->
+                    val spotifyIntent = packageManager.getLaunchIntentForPackage("com.spotify.music")
+                    startActivity(spotifyIntent)
+                    dialog.dismiss()
+                }
+                alert.setNegativeButton("Close") {dialog, which ->
+                    dialog.dismiss()
+                }
+            }
+
+            is UserNotAuthorizedException -> {
+                alert.setMessage("Please agree to the listed terms.")
+                alert.setPositiveButton("Retry") { dialog, which ->
+                    authorizeUser()
+                    dialog.dismiss()
+                }
+                alert.setNegativeButton("Cancel") {dialog, which ->
+                    dialog.dismiss()
+                }
+            }
+
+        }
+        alert.create().show()
+    }
+
+    fun authorizeUser() {
+        val connectionParams = ConnectionParams.Builder(clientId)
+            .setRedirectUri(redirectUri)
+            .showAuthView(true)
+            .build()
+
 
             SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
                 override fun onConnected(p0: SpotifyAppRemote?) {
@@ -38,14 +87,10 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onFailure(p0: Throwable?) {
                     Log.e(TAG, "connection failed $p0")
+                    handleException(p0 as SpotifyAppRemoteException)
                 }
             })
-        }
-
-
-
     }
-
     private fun connected() {
         // Then we will write some more code here.
         spotifyAppRemote?.let {
