6313b46df285eac5946c1f600ad3da9a1e616b3d
AHUYNH00 <AHUYNH00@UW.EDU>
2023-12-05 01:04:03 -0800

    search layout draft

---
+++ app/src/main/java/edu/ischool/lton2/tunesmith/search.kt
@@ -0,0 +1,11 @@
+package edu.ischool.lton2.tunesmith
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class search : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
