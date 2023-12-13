30eb819f02123f47460dd4d49dc75bc02d6479ee
AHUYNH00 <AHUYNH00@UW.EDU>
2023-12-12 23:24:13 -0800

    fixed search page formatting

--- app/src/main/java/edu/ischool/lton2/tunesmith/SearchActivity.kt
+++ app/src/main/java/edu/ischool/lton2/tunesmith/SearchActivity.kt
@@ -8,6 +8,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ListView
 import androidx.appcompat.widget.SearchView
 import androidx.core.content.edit
 import androidx.core.view.MenuItemCompat
@@ -30,7 +32,6 @@ class SearchActivity : AppCompatActivity() {
     lateinit var sharedPref: SharedPreferences
 
     val networkThread = Executors.newSingleThreadExecutor()
-    //TODO: make UI thread
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -55,56 +56,45 @@ class SearchActivity : AppCompatActivity() {
             }
 
         }
+        Log.i(TAG, "search activity created")
 
-        val testSearch = findViewById<Button>(R.id.search_btn)
-        testSearch.setOnClickListener{
-            networkThread.execute{
-                try {
-                    searchSong("blah")
-                } catch(err: Exception){
-                    Log.d(TAG, err.toString())
-                }
-            }
-
-        }
+//        val testSearch = findViewById<Button>(R.id.search_btn)
+//        testSearch.setOnClickListener{
+//            networkThread.execute{
+//                try {
+//                    searchSong("blah")
+//                } catch(err: Exception){
+//                    Log.d(TAG, err.toString())
+//                }
+//            }
+//
+//        }
 
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate menu with items using MenuInflator
-        val inflater = menuInflater
-        inflater.inflate(R.menu.top_menu, menu)
+        Log.i(TAG, "oncreateoptionsmenu")
+        menuInflater.inflate(R.menu.top_menu, menu)
 
-        // Initialise menu item search bar
-        // with id and take its object
-//        val searchViewItem = menu.findItem(R.id.search_bar)
-//        val searchView = MenuItemCompat.getActionView(searchViewItem) as SearchView
-        val searchView = menu.findItem(R.id.search_bar)?.actionView as? SearchView
-
-        // attach setOnQueryTextListener
-        // to search view defined above
+        val searchViewItem = menu.findItem(R.id.search_bar)
+        val searchView = MenuItemCompat.getActionView(searchViewItem) as SearchView
+        Log.i(TAG, "searchView: " + searchView.toString())
 
         searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            // Override onQueryTextSubmit method which is call when submit query is searched
             override fun onQueryTextSubmit(query: String): Boolean {
-                // If the list contains the search query than filter the adapter
-                // using the filter method with the query as its argument
-//                if (mylist.contains(query)) {
-//                    adapter.filter.filter(query)
-//                } else {
-//                    // Search query not found in List View
-//                    Toast.makeText(this@MainActivity, "Not found", Toast.LENGTH_LONG).show()
-//                }
+                Log.i(TAG, "submitted query")
+                searchSong("bkah")
+                Log.i(TAG, "searched")
                 return false
             }
 
-            // This method is overridden to filter the adapter according
-            // to a search query when the user is typing search
             override fun onQueryTextChange(newText: String): Boolean {
+                Log.i(TAG, "textchaange")
 //                adapter.filter.filter(newText)
                 return false
             }
         })
+        Log.i(TAG, "end of oncreateoptionsmenu")
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -112,47 +102,61 @@ class SearchActivity : AppCompatActivity() {
         Log.i(TAG, "User token: ${sharedPref.getString("AccessToken", "")}")
         val fillerQuery = "White Lie"
         var searchUrl = URL("https://api.spotify.com/v1/search?q=$fillerQuery&type=track&limit=2")
-        var urlConnection = searchUrl.openConnection() as HttpURLConnection
-        Log.i(TAG, "searching for tracks matching query")
-        urlConnection.setRequestProperty("Authorization", "Bearer ${sharedPref.getString("AccessToken", "")}")
-        val checkAuth = urlConnection.getRequestProperty("Authorization")
-        Log.i(TAG, "check url auth token: ${checkAuth}")
 
-        val inputStream = urlConnection.inputStream
+        networkThread.execute{
+            try {
+                var urlConnection = searchUrl.openConnection() as HttpURLConnection
+                Log.i(TAG, "searching for tracks matching query")
+                urlConnection.setRequestProperty("Authorization", "Bearer ${sharedPref.getString("AccessToken", "")}")
+                val checkAuth = urlConnection.getRequestProperty("Authorization")
+                Log.i(TAG, "check url auth token: ${checkAuth}")
 
-        val reader  = InputStreamReader(inputStream)
-        var tracks: JSONArray
-        reader.use {
-            val json = JSONObject(it.readText())
-            tracks = json.getJSONObject("tracks").getJSONArray("items")
-            Log.i(TAG, "search results: $tracks")
+                val inputStream = urlConnection.inputStream
+
+                val reader  = InputStreamReader(inputStream)
+                var tracks: JSONArray
+                reader.use {
+                    val json = JSONObject(it.readText())
+                    tracks = json.getJSONObject("tracks").getJSONArray("items")
+                    Log.i(TAG, "search results: $tracks")
+                }
+                var songResults: MutableList<Song> = mutableListOf()
+                // grab "name", duration "duration_ms", "artists" "name" (nested)
+                for (i in 0 until tracks.length()) {
+                    val track = tracks.getJSONObject(i)
+                    Log.i(TAG, "test iterate: $track")
+                    Log.i(TAG, "track name: ${track.getString("name")}")
+                    Log.i(TAG, "duration: ${track.getInt("duration_ms")}")
+                    Log.i(TAG, "song id: ${track.getString("id")}")
+                    val artistObj = track.getJSONArray("artists")
+
+                    var artistName = artistObj.getJSONObject(0).getString("name")
+                    for (j in 1 until artistObj.length()) {
+                        artistName += ", " + artistObj.getJSONObject(j).getString("name")
+                    }
+                    Log.i(TAG, "artist: $artistName")
+
+                    // missing id rn
+                    val trackData = Song(
+                        track.getString("name"),
+                        artistName,
+                        "",
+                        track.getInt("duration_ms").toString()
+                    )
+                    songResults.add(trackData)
+                }
+                Log.i(TAG, songResults.toString())
+            } catch(error: Exception) {
+                Log.d(TAG, error.toString())
+            }
         }
 
-        var songResults: MutableList<HomeActivity.Song> = mutableListOf()
-        // grab "name", duration "duration_ms", "artists" "name" (nested)
-        for (i in 0 until tracks.length()) {
-            val track = tracks.getJSONObject(i)
-            Log.i(TAG, "test iterate: $track")
-            Log.i(TAG, "track name: ${track.getString("name")}")
-            Log.i(TAG, "duration: ${track.getInt("duration_ms")}")
-            Log.i(TAG, "song id: ${track.getString("id")}")
-            val artistObj = track.getJSONArray("artists")
+    }
 
-            var artistName = artistObj.getJSONObject(0).getString("name")
-            for (j in 1 until artistObj.length()) {
-                artistName += ", " + artistObj.getJSONObject(j).getString("name")
-            }
-            Log.i(TAG, "artist: $artistName")
-//            Log.i(TAG, "track name: ${track.getString("name")}")
-//            val song = HomeActivity.Song(
-//                track.getString("name"),
-//                track.getJSONArray("artists").getJSONObject(0).getString("name"),
-//                track.getString("id")
-//            )
-//            songResults.add(song)
-
+    private fun displayResults(trackList: MutableList<Song>) {
+        this.runOnUiThread{
+            val listView = findViewById<ListView>(R.id.listView)
+//            listView.adapter = PlaylistAdapter(trackList)
         }
     }
 }
-//TODO: extract to be used across all activities
-data class Track(val name: String, val artists: String, val trackId: String, val durationMs: Int)
