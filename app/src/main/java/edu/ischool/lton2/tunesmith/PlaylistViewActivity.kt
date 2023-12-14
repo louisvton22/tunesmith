cce19a8b1c46904b29b1ddf4b0c09dbbfe1977b1
louisvton22 <81780806+louisvton22@users.noreply.github.com>
2023-12-14 15:22:59 -0800

    reduced image quality for more image support

--- app/src/main/java/edu/ischool/lton2/tunesmith/PlaylistViewActivity.kt
+++ app/src/main/java/edu/ischool/lton2/tunesmith/PlaylistViewActivity.kt
@@ -249,11 +249,11 @@ class PlaylistViewActivity : AppCompatActivity(), NavBar,  PlaylistAdapter.OnSon
                     //Log.i(TAG, playlist?.image)
                     val bitmap = BitmapFactory.decodeFile(playlist?.image)
                     val byteArrayOutputStream = ByteArrayOutputStream()
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream)
                     val byteArray = byteArrayOutputStream.toByteArray()
                     var encoded = Base64.encodeToString(byteArray, Base64.DEFAULT)
                     encoded = encoded.replace("\n", "").replace("\r", "")
-                    //Log.i(TAG, encoded)
+                    Log.i(TAG, "Encoded file size: ${encoded.toByteArray().size}")
 
                     val outputStreamWriter = OutputStreamWriter(urlConnection.outputStream)
                     outputStreamWriter.write(encoded)
